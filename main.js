1da7530b8f8d63915f890e62f071fd19309758aa
lrs <99574908+lrsgzs@users.noreply.github.com>
2023-05-27 21:07:08 +0800

    short link

--- main.js
+++ main.js
@@ -20,34 +20,58 @@ function getDecode(str){
 }
 
 var data = GetRequest();
-var type = data["type"];
+var type = data["t"];
 if (type == null) {
     document.getElementById("message").innerHTML = "跳转失败！";
 } else if (type == "bv") {
-    var id = data["id"];
+    var id = data["i"];
     if (id == null) {
         document.getElementById("message").innerHTML = "跳转失败！";
     } else {
         location.href = "https://www.bilibili.com/video/" + id;
     }
-} else if (type == "xescode") {
-    var id = data["id"];
-    var code_type = data["code_type"];
-    if (id == null || code_type == null) {
+} else if (type == "xcc") {
+    var id = data["i"];
+    if (id == null) {
         document.getElementById("message").innerHTML = "跳转失败！";
     } else {
-        if (code_type == "cpp") {
-            location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=cpp&form=cpp&langType=cpp";
-        } else if (code_type == "webpy") {
-            location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=webpy&form=python&langType=python";
-        } else if (code_type == "offpy") {
-            location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=offline&form=python&langType=python";
-        } else if (code_type == "python") {
-            location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=python&form=python&langType=python";
-        } else if (code_type == "scratch") {
-            location.href = "https://code.xueersi.com/home/project/detail?lang=scratch&pid=" + id + "&version=3.0&langType=scratch";
-        } else {
-            document.getElementById("message").innerHTML = "跳转失败！";
-        }
+        location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=cpp&form=cpp&langType=cpp";
     }
+} else if (type == "xcw") {
+    var id = data["i"];
+    if (id == null) {
+        document.getElementById("message").innerHTML = "跳转失败！";
+    } else {
+        location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=webpy&form=python&langType=python";
+    }
+} else if (type == "xco") {
+    var id = data["i"];
+    if (id == null) {
+        document.getElementById("message").innerHTML = "跳转失败！";
+    } else {
+        location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=offline&form=python&langType=python";
+    }
+} else if (type == "xcp") {
+    var id = data["i"];
+    if (id == null) {
+        document.getElementById("message").innerHTML = "跳转失败！";
+    } else {
+        location.href = "https://code.xueersi.com/home/project/detail?lang=code&pid=" + id + "&version=python&form=python&langType=python";
+    }
+} else if (type == "xcs") {
+    var id = data["i"];
+    if (id == null) {
+        document.getElementById("message").innerHTML = "跳转失败！";
+    } else {
+        location.href = "https://code.xueersi.com/home/project/detail?lang=scratch&pid=" + id + "&version=3.0&langType=scratch";
+    }
+} else if (type == "xcsp") {
+    var id = data["i"];
+    if (id == null) {
+        document.getElementById("message").innerHTML = "跳转失败！";
+    } else {
+        location.href = "https://code.xueersi.com/space/" + id;
+    }
+} else {
+    document.getElementById("message").innerHTML = "跳转失败！";
 }
